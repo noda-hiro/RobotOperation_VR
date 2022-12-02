7444cd5a27502dc87b170bb49aae42a54c03d959
noda-hiro <hiro13424@gmail.com>
2022-12-02 12:01:47 +0900

    feat : コントローラー入力テスト用の処理を追加

--- RobotOperation_VR/Assets/Scripts/Player/Action/PlayerMove.cs
+++ RobotOperation_VR/Assets/Scripts/Player/Action/PlayerMove.cs
@@ -3,45 +3,38 @@ using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-     private float speed = 300f;
-     private Vector3 currentPos;
-     private Gamepad currentPad;
-     private Rigidbody rd;
+    private float speed = 300f;
+    private Vector3 currentPos;
+    private Gamepad currentPad;
+    private Joystick currentStick;
+    private Rigidbody rd;
 
-     // Start is called before the first frame update
-     private void Start()
-     {
-          rd = GetComponent<Rigidbody>();
-          currentPad = Gamepad.current;
-     }
+    private void Start()
+    {
+        // rd = GetComponent<Rigidbody>();
+        currentPad = Gamepad.current;
+        currentStick = Joystick.current;
+    }
 
-     // Update is called once per frame
-     private void FixedUpdate()
-     {
-          UpdateCurrentPos(GetGameStickValue());
-     }
+    private void FixedUpdate()
+    {
+        Debug.Log(currentStick.stick.up.ReadValue()+"UP");
+        Debug.Log(currentStick.stick.down.ReadValue()+"DOWN");
+        Debug.Log(currentStick.stick.right.ReadValue()+"RIGHT");
+        Debug.Log(currentStick.stick.left.ReadValue()+"LEFT");
+    }
 
-     /// <summary>
-     /// 引数の値を自身の座標に加える
-     /// </summary>
-     /// <param name="gamePadValue">GamePadの入力値</param>
-     private void UpdateCurrentPos(Vector3 gamePadValue)
-     {
-          Vector3 pos;
-          pos.x = currentPos.x + gamePadValue.x;
-          pos.y = currentPos.y;
-          pos.z = currentPos.z + gamePadValue.y;
-          rd.velocity = pos * speed * Time.deltaTime;
-     }
-
-     /// <summary>
-     /// GamePadの入力値を取得する
-     /// </summary>
-     /// <returns></returns>
-     private Vector3 GetGameStickValue()
-     {
-          Vector2 stickValue = currentPad.leftStick.ReadValue();
-          return stickValue;
-     }
+    /// <summary>
+    /// 引数の値を自身の座標に加える
+    /// </summary>
+    /// <param name="gamePadValue">GamePadの入力値</param>
+    private void UpdateCurrentPos(Vector3 gamePadValue)
+    {
+        Vector3 pos;
+        pos.x = currentPos.x + gamePadValue.x;
+        pos.y = currentPos.y;
+        pos.z = currentPos.z + gamePadValue.y;
+        rd.velocity = pos * speed * Time.deltaTime;
+    }
 
 }
